ff50ba998a315fd0da5bef84ff20fa2af8e63f33
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-16 22:18:24 +0200

    update repogen

--- repogen.sh
+++ repogen.sh
@@ -4,4 +4,7 @@
 # $2 Version
 
 myDir=$(dirname "$0")
-"$myDir/../QtModules/repogen.py" "$1" BackgroundProcess "" "" "A Library to create background applications with simple, automated foreground control." "$2" "$myDir/LICENSE" BSD-3-Clause "android_armv7,android_x86,ios,winrt_x86_msvc2017,winrt_x64_msvc2017"
+qtDir=${1?First parameter must be set to the dir to install}
+version=${2?Set the version as second parameter}
+
+"$myDir/../QtModules/deploy/repogen.py" "$qtDir" BackgroundProcess "" "" "A Library to create background applications with simple, automated foreground control." "$version" "$myDir/LICENSE" BSD-3-Clause "android_armv7,android_x86,ios,winrt_x86_msvc2017,winrt_x64_msvc2017,winrt_armv7_msvc2017"
